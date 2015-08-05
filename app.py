815bd8eb3bfafda55004e36e0a257f2b303bf1df
Taras <taras@localhost.localdomain>
2015-08-05 11:17:40 -0400

    more expressive

--- app.py
+++ app.py
@@ -2,17 +2,30 @@ from flask import Flask, render_template, request, flash, url_for, redirect, mak
 
 app = Flask(__name__)
 
+def create_response(text):
+    resp = make_response(redirect(url_for("form")))
+    resp.set_cookie("value", text)
+    return resp
+
+
+def executor(*data):
+    def wrapper(*functions):
+        for func in functions:
+            yield func(*data)
+    return wrapper
+
+
 @app.route("/form", methods=['GET'])
 def form():
     return render_template("form.html", cookies=request.cookies)
 
+
 @app.route("/form", methods=['POST'])
 def form_process():
-    text = request.form.get("subject", "Oops")
-    resp = make_response(redirect(url_for("form")))
-    resp.set_cookie("value", text)
-    flash(text)
+    bar = executor(request.form.get("subject", "Oops"))
+    resp, _ = bar(create_response, flash)
     return resp
+
 
 if __name__ == '__main__':
     app.config.from_pyfile("config.py")

1ac2ad17dd97afa1fef6c4a761299956ab154181
Taras <taras@localhost.localdomain>
2015-08-05 10:57:10 -0400

    play around cookies

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, request, flash, url_for, redirect
+from flask import Flask, render_template, request, flash, url_for, redirect, make_response
 
 app = Flask(__name__)
 
 @app.route("/form", methods=['GET'])
 def form():
-    return render_template("form.html")
+    return render_template("form.html", cookies=request.cookies)
 
 @app.route("/form", methods=['POST'])
 def form_process():
-    flash(request.form.get("subject", "Oops"))
-    return redirect(url_for("form"))
+    text = request.form.get("subject", "Oops")
+    resp = make_response(redirect(url_for("form")))
+    resp.set_cookie("value", text)
+    flash(text)
+    return resp
 
 if __name__ == '__main__':
     app.config.from_pyfile("config.py")
